9c6aae9f82e003c9346da9079f53b59bd27e0e40
CsongorVincze <vinczefarkascsongor@gmail.com>
2026-03-04 10:56:50 +0100

    2repcsi

--- ULTRAHANG/time_of_flight.py
+++ ULTRAHANG/time_of_flight.py
@@ -20,15 +20,18 @@ plt.rcParams.update({
 # Távolság módosítás [mm]
 distance_mm = np.array([0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24])
 
-# Repülési idő (Time of flight) [us]
-time_ms = np.array([0.565, 0.571, 0.578, 0.582, 0.588, 0.594, 0.600, 0.606, 0.611, 0.617, 0.623, 0.629, 0.634])
-
+# Repülési idő (Time of flight) [ms]
+time_ms_1 = np.array([0.565, 0.571, 0.578, 0.582, 0.588, 0.594, 0.600, 0.606, 0.611, 0.617, 0.623, 0.629, 0.634])
+time_ms_2 = np.array([0.564, 0.571, 0.577, 0.582, 0.587, 0.592, 0.599, 0.605, 0.610, 0.616, 0.623, 0.628, 0.633 ])
 
 # --- ÁBRÁZOLÁS ---
 plt.figure(figsize=(8, 6))
 
-if len(distance_mm) > 0 and len(time_ms) > 0:
-    plt.plot(distance_mm, time_ms, 'o', color='#1f77b4', markersize=8, label="Mért repülési idők")
+if len(distance_mm) > 0:
+    if len(time_ms_1) == len(distance_mm):
+        plt.plot(distance_mm, time_ms_1, 'o', color='#1f77b4', markersize=8, label="1. Mérés")
+    if len(time_ms_2) == len(distance_mm):
+        plt.plot(distance_mm, time_ms_2, 's', color='#ff7f0e', markersize=8, label="2. Mérés", alpha=0.8)
 
 plt.title('Repülési idő a relatív távolság függvényében')
 plt.xlabel(r'Relatív távolság, $\Delta s$ [mm]')
@@ -36,7 +39,7 @@ plt.ylabel(r'Repülési idő, $t$ [ms]')
 plt.grid(True, linestyle='--', alpha=0.7)
 
 # Csak akkor jelenítjük meg a jelmagyarázatot, ha van mit
-if len(distance_mm) > 0 and len(time_ms) > 0:
+if (len(time_ms_1) == len(distance_mm)) or (len(time_ms_2) == len(distance_mm)):
     plt.legend()
 
 plt.tight_layout()
