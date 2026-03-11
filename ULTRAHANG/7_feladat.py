365b05ba77a228b49574a2428ed1080fbca098e4
CsongorVincze <vinczefarkascsongor@gmail.com>
2026-03-11 11:03:18 +0100

    7meresadatok

---
+++ ULTRAHANG/7_feladat.py
@@ -0,0 +1,8 @@
+#geometriai adatok:
+#vevo-fixtukor: 19.5cm
+#vevo-feligtukor: 10.5cm
+#ado-mozgtuk: 19.5cm
+#ado-feligtukor:10cm
+
+dist = np.array([23.59, 20.44, 18.96, 15.50, 14.74, 12.17, 10.22, 8.05, 6.50, 3.65, 2.05 ])
+amp = mp.array([93.0, 45.0, 102.0, 34.0, 104.0, 42.0, 106.0, 34.0, 103.0, 28.0, 115.0 ])
