cbb102319feedd4850729b39fde8091a7623fc7b
CsongorVincze <vinczefarkascsongor@gmail.com>
2026-03-11 11:27:10 +0100

    8feladat

---
+++ ULTRAHANG/8_feladat.py
@@ -0,0 +1,7 @@
+#14cm az adok es a vevo tavolsaga
+# megkerestuk itt 668.0mv 19.4cm az orso allasa
+#60fokot beallitva a maximalis amplitudot 1.90cm orsoallasnal tapasztaltuk. Ez: 764.0mV -nak adodott.
+
+#120fokot beallitva a maximalis amplitudot 4.94cm orsoallasnal tapasztaltuk. 787.5mV -nak adodott.
+
+#180 fokot beallitva a maximalis amplitudot 8.15cm orsoallasnal tapsztaltuk. 775.0mV -nak adodott.
