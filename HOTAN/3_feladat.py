bdfccba1c4bdabc20cf151d770a06d6b67d2504a
CsongorVincze <vinczefarkascsongor@gmail.com>
2026-03-25 10:27:26 +0100

    3_feladat

--- HOTAN/3_feladat.py
+++ HOTAN/3_feladat.py
@@ -1,7 +1,10 @@
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-filename = "3_feladat.csv"
+# Get the absolute path to the directory containing this script
+script_dir = os.path.dirname(os.path.abspath(__file__))
+filename = os.path.join(script_dir, 'adatok', '3_feladat.csv')
 
 try:
     data = np.genfromtxt(filename, delimiter=',', skip_header=0)
@@ -27,3 +30,6 @@ plt.legend()
 plt.tight_layout()
 plt.savefig('3_1_graf.pdf')
 plt.show()
+
+print(f"ez a meredekseg {m}")
+print(f"delta T: {temp[-1] - temp[0]}")
